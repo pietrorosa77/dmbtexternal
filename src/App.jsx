68d9f6a11b185c43c60536e12eaacc33fcd43eba
Pietro Rosa <pietrorosa77@gmail.com>
2023-04-29 22:31:52 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -6,5 +6,17 @@ export default () => {
   <>
     <h1>Welcome to React Vite Micro App!</h1>
     <p>Hard to get more minimal than this React app.</p>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
   </>
 );}
