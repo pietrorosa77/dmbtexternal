6f6b4b0336323440499dec70e63600a9d1514355
Pietro Rosa <pietrorosa77@gmail.com>
2023-04-30 23:54:07 +0200

    test

--- src/definition.jsx
+++ src/definition.jsx
@@ -9,6 +9,14 @@ window.ExternalDummbotTest = ExternalDummbotTest;
 const onResize = () => {requestAnimationFrame(() => {
   window.xprops._onChangeSize(document.documentElement.getBoundingClientRect().height);
 })}
+
+const cssString = window.xprops._onGetCss();
+const head = document.head || document.getElementsByTagName('head')[0];
+const style = document.createElement('style');
+head.appendChild(style);
+style.type = 'text/css';
+style.appendChild(document.createTextNode(cssString));
+
 window.addEventListener("DOMContentLoaded", () => {
   window.xprops._onLoaded(document.documentElement.getBoundingClientRect().height);
   window.xprops._onChangeSize(document.documentElement.getBoundingClientRect().height)
